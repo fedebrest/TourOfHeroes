36aae56e0481a1d988ec09450c4dff15c0f0e1da
Fede <federiconbrest@gmail.com>
2016-09-15 12:58:37 -0300

    Cambie el titulo del Template

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Detalles de{{title}}</h1><h2>{{hero}}!</h2>'
+    template: '<h1>{{title}}</h1><h2>Detalles de {{hero}}!</h2>'
 })
 export class AppComponent { 
 	title = 'Tour of Heroes';
